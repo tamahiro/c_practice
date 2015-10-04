8aa4d5a2677b15ba6a4e10e7fd029f717a1c67f6
tamahiro <tmkshrnr@gmail.com>
2015-10-04 15:07:12 +0900

    add signal handler

--- chap16/httpd.c
+++ chap16/httpd.c
@@ -18,3 +18,24 @@ static void *xalloc(size_t sz)
   return p;
 }
 
+static void install_signal_handlers(void)
+{
+  trap_signal(SIGPIPE, signal_exit);
+}
+
+static void trap_signal(int sig, sighander_t handler)
+{
+  struct sigaction act;
+
+  act.sa_handler = handler;
+  sigemptyset(&act.sa_mark);
+
+  act.sa_flags = SA_RESTART;
+  if(sigaction(sig, &act, NULL) < 0)
+    log_exit("sigaction() failed: %s", sterror(errno);
+}
+
+static void signal_exit(int sig)
+{
+  log_exit("exit by signal %d", sig);
+}
